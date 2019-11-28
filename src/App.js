2d0be1280338c5b9151482ca3e6b8d9b4fa2df29
Vincent Dang <vncntdang@gmail.com>
2019-11-28 12:36:12 -0500

    test gh pages :trollface:

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default class App extends Component {
     render() {
         return (
             <div className="App">
-                    
+                  <h1>H-L Index</h1>
             </div>
         );
     }
